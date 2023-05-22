2a42930af2c52632a76aadf07b0ed6c57468a97c
jkmrto <juanka1331@gmail.com>
2023-05-23 01:36:03 +0200

    setup lua LSP

--- .config/nvim/lua/lsp.lua
+++ .config/nvim/lua/lsp.lua
@@ -1,6 +1,5 @@
 -- on_attach=require'completion'.on_attach,
 local lsp = require'lspconfig'
-local comp = require'completion'
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities.textDocument.completion.completionItem.snippetSupport = true
 
@@ -20,5 +19,28 @@ lsp.cssls.setup {
 lsp.html.setup {
 	filetypes = { "html", "eex" }
 }
-lsp.vimls.setup{}	
+lsp.vimls.setup{}
 
+
+require'lspconfig'.lua_ls.setup {
+  settings = {
+    Lua = {
+      runtime = {
+        -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+        version = 'LuaJIT',
+      },
+      diagnostics = {
+        -- Get the language server to recognize the `vim` global
+        globals = {'vim'},
+      },
+      workspace = {
+        -- Make the server aware of Neovim runtime files
+        library = vim.api.nvim_get_runtime_file("", true),
+      },
+      -- Do not send telemetry data containing a randomized but unique identifier
+      telemetry = {
+        enable = false,
+      },
+    },
+  },
+}
